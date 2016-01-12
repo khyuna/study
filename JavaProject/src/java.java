95bd33105b066f33e151188c162afabdcfe63443
khyuna <khyun9764@naver.com>
2016-01-12 22:34:59 +0900

    Update java.java

--- JavaProject/src/java.java
+++ JavaProject/src/java.java
@@ -5,5 +5,6 @@ public static void main(String [] args){
 	System.out.println("Add Java Class File Push Again");
 	System.out.println("Add your comment");
 	System.out.println("ADDDD");
+	System.out.println("ADDDDDD");
 }
 }
